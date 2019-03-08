3f07e8180104b12dc53957852e57b31137f45de8
xuyating156 <837356466@qq.com>
2019-03-08 17:17:42 +0800

    scott.sql

--- ip.sql
+++ ip.sql
@@ -1,18 +1,28 @@
-drop database  if exists db_ip;
-create  database  db_ip;
+drop database if exists db_ip;
+create database db_ip;
 
-drop table  if exists db_ip.ip;
-create table db_ip.ip(
-  id int auto_increment primary key comment  'id pk',
- min varchar(15) not null comment 'min id NN',
- max varchar(15) not null comment 'min id NN',
-  loc varchar(15) not null comment 'min id NN'
+drop table if exists db_ip.ip;
+create table db_ip.ip
+(
+  id  int auto_increment primary key comment 'id PK',
+  min varchar(255) not null comment 'min ip NN',
+  max varchar(255) not null comment 'max ip NN',
+  loc varchar(255) not null comment 'location NN'
+) comment 'ip address table';
 
-);
+load data local infile 'd:/ip.txt'
+  into table db_ip.ip
+  fields terminated by '|'
+  (min, max, loc)
+  set id = null;
+
 select *
 from db_ip.ip;
-load data local infile 'd:/ip.txt'
-into table dp_ip.ip
-fields terminated by '|'
-  (min,max,loc)
-set id =null;
+
+select *
+from db_ip.ip;
+
+update db_ip.ip
+set min = trim(min),
+    max = trim(max),
+    loc = trim(loc);-- 把空格替换成min max loc
