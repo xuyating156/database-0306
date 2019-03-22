b69b832c67d5b024fcbd2df1142a947f8f010dde
xuyating156 <837356466@qq.com>
2019-03-22 17:10:59 +0800

    创建线程 阻塞函数

--- 20190322.c
+++ 20190322.c
@@ -3,14 +3,22 @@
 
 void * pthread1(void*a)
 {
+printf("hello\r\n");
 return NULL;
 }
+
 int main(int argc,char **argv)
 {
    pthread_t id=0;
    if(0!=pthread_create(&id,NULL,pthread1,NULL))
     {
     printf("pthread_create faile.\r\n");
+     return 0;
+    }
+
+   if(-1== phread_join(id,NULL))
+    {
+    printf("pthread_join faile.\r\n");
      return 0;
     }
 return 0;
